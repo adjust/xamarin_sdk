f7bd6b7af0ce90ada9979a3279f1646ca3bb71b7
uerceg <ugljesa@adjust.com>
2015-11-02 14:42:40 +0100

    Making iAd and AdSupport weak frameworks

--- AdjustBindingsiOS/libAdjust.linkwith.cs
+++ AdjustBindingsiOS/libAdjust.linkwith.cs
@@ -1,3 +1,3 @@
 using ObjCRuntime;
 
-[assembly: LinkWith ("libAdjust.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = false, Frameworks = "AdSupport iAd")]
+[assembly: LinkWith ("libAdjust.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator64 | LinkTarget.Arm64, SmartLink = true, ForceLoad = false, WeakFrameworks = "AdSupport iAd")]
