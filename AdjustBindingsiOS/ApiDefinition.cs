8d0a36459b62768fa3cd09ce839a5a4e91770412
uerceg <ugljesa@adjust.com>
2015-11-04 12:48:16 +0100

    Moving log enum to StructsAndEnums.cs

--- AdjustBindingsiOS/ApiDefinition.cs
+++ AdjustBindingsiOS/ApiDefinition.cs
@@ -300,14 +300,4 @@ namespace AdjustBindingsiOS
         [Export ("getInstance")]
         NSObject Instance { get; }
     }
-
-    public enum ADJLogLevel : uint
-    {
-        Verbose = 1,
-        Debug = 2,
-        Info = 3,
-        Warn = 4,
-        Error = 5,
-        Assert = 6
-    }
 }
---
+++ AdjustBindingsiOS/StructsAndEnums.cs
@@ -0,0 +1,11 @@
+namespace AdjustBindingsiOS {
+    public enum ADJLogLevel : uint
+    {
+        Verbose = 1,
+        Debug = 2,
+        Info = 3,
+        Warn = 4,
+        Error = 5,
+        Assert = 6
+    }
+}
