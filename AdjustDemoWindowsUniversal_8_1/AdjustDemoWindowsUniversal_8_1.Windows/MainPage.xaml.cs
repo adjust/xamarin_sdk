684b108c3a5b270176260c525e3f0dd5bcf66b7f
Pedro Filipe <pedro@adeven.com>
2015-11-27 12:25:55 +0100

    Add demo WU 8.1

---
+++ AdjustDemoWindowsUniversal_8_1/AdjustDemoWindowsUniversal_8_1.Windows/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using AdjustSdk;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace AdjustDemoWindowsUniversal_8_1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnSimpleEvent_Click(object sender, RoutedEventArgs e)
+        {
+            var simpleEvent = new AdjustEvent("{yourSimpleEventToken}");
+            Adjust.TrackEvent(simpleEvent);
+        }
+
+        private void btnRevenueEvent_Click(object sender, RoutedEventArgs e)
+        {
+            var revenueEvent = new AdjustEvent("{yourRevenueEventToken}");
+            revenueEvent.SetRevenue(0.01, "EUR");
+            Adjust.TrackEvent(revenueEvent);
+        }
+
+        private void btnCallbakEvent_Click(object sender, RoutedEventArgs e)
+        {
+            var callbackEvent = new AdjustEvent("{yourCallbackEventToken}");
+            var pclInfo = AdjustDemoPortableLibrary.AdjustDemoPCL.Info;
+            callbackEvent.AddPartnerParameter("key", pclInfo);
+            Adjust.TrackEvent(callbackEvent);
+        }
+
+        private void btnPartnerEvent_Click(object sender, RoutedEventArgs e)
+        {
+            var partnerEvent = new AdjustEvent("{yourPartnerEventToken}");
+            partnerEvent.AddPartnerParameter("foo", "bar");
+            Adjust.TrackEvent(partnerEvent);
+        }
+    }
+}
