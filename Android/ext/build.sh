4f46a3ecd3edffce5a3a565ea7d6d980f0d6f5f5
Obaied <abdullah.obaied@gmail.com>
2018-01-26 14:11:10 +0100

    Append/revert sdkPrefix from source

--- Android/ext/build.sh
+++ Android/ext/build.sh
@@ -4,7 +4,7 @@
 # - Copy the JAR file to the root dir
 
 # End script if one of the lines fails
- set -e
+set -e
 
 if [ $# -ne 1 ]; then
     echo $0: "usage: ./build.sh [debug || release]"
@@ -21,12 +21,21 @@ ROOT_DIR="$(dirname "$ROOT_DIR")"
 ROOT_DIR="$(dirname "$ROOT_DIR")"
 JAR_IN_DIR=Android/ext/sdk/Adjust/adjust/build/outputs
 JAR_OUT_DIR=Android/AdjustSdk.Xamarin.Android/Jars
+PROJECT_DIR=Android/ext/sdk/Adjust
+
+SDK_PREFIX='xamarin4.12.0'
 
 RED='\033[0;31m' # Red color
 GREEN='\033[0;32m' # Green color
 NC='\033[0m' # No Color
 
-cd ${ROOT_DIR}/Android/ext/sdk/Adjust
+echo -e "${GREEN}>>> Appending prefix to source ${NC}"
+cd ${ROOT_DIR}/${PROJECT_DIR}
+grep -r 'adjustInstance.onCreate(adjustConfig)' -l --null . | LC_ALL=C xargs -0 sed -i '' "s#adjustInstance.onCreate(adjustConfig);#adjustConfig.setSdkPrefix(\"${SDK_PREFIX}\");adjustInstance.onCreate(adjustConfig);#g"
+echo success
+
+echo -e "${GREEN}>>> Running Gradle ${NC}"
+cd ${ROOT_DIR}/${PROJECT_DIR}
 
 if [ "$BUILD_TYPE" == "debug" ]; then
     echo -e "${GREEN}>>> Running Gradle tasks: makeDebugJar${NC}"
@@ -36,6 +45,11 @@ elif [ "$BUILD_TYPE" == "release" ]; then
     echo -e "${GREEN}>>> Running Gradle tasks: makeReleaseJar${NC}"
     ./gradlew clean makeReleaseJar
 fi
+echo success
+
+echo -e "${GREEN}>>> Removing prefix from source ${NC}"
+cd ${ROOT_DIR}/${PROJECT_DIR}
+grep -r 'adjustInstance.onCreate(adjustConfig)' -l --null . | LC_ALL=C xargs -0 sed -i '' "s#adjustConfig.setSdkPrefix(\"${SDK_PREFIX}\");adjustInstance.onCreate(adjustConfig);#adjustInstance.onCreate(adjustConfig);#g"
 echo success
 
 echo -e "${GREEN}>>> Moving the jar from ${JAR_IN_DIR} to ${JAR_OUT_DIR} ${NC}"
