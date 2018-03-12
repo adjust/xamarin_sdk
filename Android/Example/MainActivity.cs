eb3421a43c5c65dd6e71017d15869f5ca1a3e4e3
uerceg <ugi@adjust.com>
2018-03-12 17:31:58 +0100

    Example apps cleanup

--- Android/Example/MainActivity.cs
+++ Android/Example/MainActivity.cs
@@ -8,103 +8,103 @@ using System;
 
 namespace Example
 {
-	[Activity(Label = "Example", MainLauncher = true)]
-	[IntentFilter
-	 (new[] { Intent.ActionView },
-		Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
-		DataScheme = "adjust-example")]
+    [Activity(Label = "Example", MainLauncher = true)]
+    [IntentFilter
+     (new[] { Intent.ActionView },
+        Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
+        DataScheme = "adjust-example")]
     public class MainActivity : Activity, IOnDeviceIdsRead
-	{
-		protected override void OnCreate(Bundle savedInstanceState)
-		{
-			base.OnCreate(savedInstanceState);
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
 
-			// Set our view from the "main" layout resource.
-			SetContentView(Resource.Layout.Main);
+            // Set our view from the "main" layout resource.
+            SetContentView(Resource.Layout.Main);
 
-			Intent intent = this.Intent;
-			var data = intent.Data;
-			Adjust.AppWillOpenUrl(data);
+            Intent intent = this.Intent;
+            var data = intent.Data;
+            Adjust.AppWillOpenUrl(data);
 
-			// Get our button from the layout resource,
-			// and attach an event to it.
-			Button btnTrackSimpleEvent = FindViewById<Button>(Resource.Id.btnTrackSimpleEvent);
-			Button btnTrackRevenueEvent = FindViewById<Button>(Resource.Id.btnTrackRevenueEvent);
-			Button btnTrackCallbackEvent = FindViewById<Button>(Resource.Id.btnTrackCallbackEvent);
-			Button btnTrackPartnerEvent = FindViewById<Button>(Resource.Id.btnTrackPartnerEvent);
-			Button btnEnableOfflineMode = FindViewById<Button>(Resource.Id.btnEnableOfflineMode);
-			Button btnDisableOfflineMode = FindViewById<Button>(Resource.Id.btnDisableOfflineMode);
-			Button btnEnableSDK = FindViewById<Button>(Resource.Id.btnEnableSDK);
-			Button btnDisableSDK = FindViewById<Button>(Resource.Id.btnDisableSDK);
-			Button btnIsSDKEnabled = FindViewById<Button>(Resource.Id.btnIsSDKEnabled);
+            // Get our button from the layout resource,
+            // and attach an event to it.
+            Button btnTrackSimpleEvent = FindViewById<Button>(Resource.Id.btnTrackSimpleEvent);
+            Button btnTrackRevenueEvent = FindViewById<Button>(Resource.Id.btnTrackRevenueEvent);
+            Button btnTrackCallbackEvent = FindViewById<Button>(Resource.Id.btnTrackCallbackEvent);
+            Button btnTrackPartnerEvent = FindViewById<Button>(Resource.Id.btnTrackPartnerEvent);
+            Button btnEnableOfflineMode = FindViewById<Button>(Resource.Id.btnEnableOfflineMode);
+            Button btnDisableOfflineMode = FindViewById<Button>(Resource.Id.btnDisableOfflineMode);
+            Button btnEnableSDK = FindViewById<Button>(Resource.Id.btnEnableSDK);
+            Button btnDisableSDK = FindViewById<Button>(Resource.Id.btnDisableSDK);
+            Button btnIsSDKEnabled = FindViewById<Button>(Resource.Id.btnIsSDKEnabled);
             Button btnGetIds = FindViewById<Button>(Resource.Id.btnGetIds);
 
-			btnTrackSimpleEvent.Click += delegate
-			{
-				AdjustEvent adjustEvent = new AdjustEvent("g3mfiw");
-				Adjust.TrackEvent(adjustEvent);
-			};
+            btnTrackSimpleEvent.Click += delegate
+            {
+                AdjustEvent adjustEvent = new AdjustEvent("g3mfiw");
+                Adjust.TrackEvent(adjustEvent);
+            };
 
-			btnTrackRevenueEvent.Click += delegate
-			{
-				AdjustEvent adjustEvent = new AdjustEvent("a4fd35");
+            btnTrackRevenueEvent.Click += delegate
+            {
+                AdjustEvent adjustEvent = new AdjustEvent("a4fd35");
 
-				// Add revenue 1 cent of an euro.
-				adjustEvent.SetRevenue(0.01, "EUR");
-				adjustEvent.SetOrderId("dummy_id");
+                // Add revenue 1 cent of an euro.
+                adjustEvent.SetRevenue(0.01, "EUR");
+                adjustEvent.SetOrderId("dummy_id");
 
-				Adjust.TrackEvent(adjustEvent);
-			};
+                Adjust.TrackEvent(adjustEvent);
+            };
 
-			btnTrackCallbackEvent.Click += delegate
-			{
-				AdjustEvent adjustEvent = new AdjustEvent("34vgg9");
+            btnTrackCallbackEvent.Click += delegate
+            {
+                AdjustEvent adjustEvent = new AdjustEvent("34vgg9");
 
-				// Add callback parameters to this parameter.
-				adjustEvent.AddCallbackParameter("a", "b");
-				adjustEvent.AddCallbackParameter("key", "value");
-				adjustEvent.AddCallbackParameter("a", "c");
+                // Add callback parameters to this parameter.
+                adjustEvent.AddCallbackParameter("a", "b");
+                adjustEvent.AddCallbackParameter("key", "value");
+                adjustEvent.AddCallbackParameter("a", "c");
 
-				Adjust.TrackEvent(adjustEvent);
-			};
+                Adjust.TrackEvent(adjustEvent);
+            };
 
-			btnTrackPartnerEvent.Click += delegate
-			{
-				AdjustEvent adjustEvent = new AdjustEvent("w788qs");
+            btnTrackPartnerEvent.Click += delegate
+            {
+                AdjustEvent adjustEvent = new AdjustEvent("w788qs");
 
-				// Add partner parameters to this parameter.
-				adjustEvent.AddPartnerParameter("x", "y");
-				adjustEvent.AddPartnerParameter("foo", "bar");
-				adjustEvent.AddPartnerParameter("x", "z");
+                // Add partner parameters to this parameter.
+                adjustEvent.AddPartnerParameter("x", "y");
+                adjustEvent.AddPartnerParameter("foo", "bar");
+                adjustEvent.AddPartnerParameter("x", "z");
 
-				Adjust.TrackEvent(adjustEvent);
-			};
+                Adjust.TrackEvent(adjustEvent);
+            };
 
-			btnEnableOfflineMode.Click += delegate
-			{
-				Adjust.SetOfflineMode(true);
-			};
+            btnEnableOfflineMode.Click += delegate
+            {
+                Adjust.SetOfflineMode(true);
+            };
 
-			btnDisableOfflineMode.Click += delegate
-			{
-				Adjust.SetOfflineMode(false);
-			};
+            btnDisableOfflineMode.Click += delegate
+            {
+                Adjust.SetOfflineMode(false);
+            };
 
-			btnEnableSDK.Click += delegate
-			{
-				Adjust.Enabled = true;
-			};
+            btnEnableSDK.Click += delegate
+            {
+                Adjust.Enabled = true;
+            };
 
-			btnDisableSDK.Click += delegate
-			{
-				Adjust.Enabled = false;
-			};
+            btnDisableSDK.Click += delegate
+            {
+                Adjust.Enabled = false;
+            };
 
-			btnIsSDKEnabled.Click += delegate
-			{
-				string message = Adjust.Enabled ? "SDK is ENABLED" : "SDK is DISABLED";
-				Toast.MakeText(this, message, ToastLength.Short).Show();
-			};
+            btnIsSDKEnabled.Click += delegate
+            {
+                string message = Adjust.Enabled ? "SDK is ENABLED" : "SDK is DISABLED";
+                Toast.MakeText(this, message, ToastLength.Short).Show();
+            };
 
             btnGetIds.Click += delegate
             {
@@ -123,29 +123,29 @@ namespace Example
                     Console.WriteLine("Attribution Adid: " + Adjust.Attribution.Adid);
                 }
             };
-		}
+        }
 
         public void OnGoogleAdIdRead(string googleAdId)
         {
             Console.WriteLine("Google Ad Id: " + googleAdId);
         }
 
-		protected override void OnResume()
-		{
-			base.OnResume();
+        protected override void OnResume()
+        {
+            base.OnResume();
 
-			// In case you are supporting API level lower than 14.
-			// Adjust.OnResume();
-		}
+            // In case you are supporting API level lower than 14.
+            // Adjust.OnResume();
+        }
 
-		protected override void OnPause()
-		{
-			base.OnPause();
+        protected override void OnPause()
+        {
+            base.OnPause();
 
-			// In case you are supporting API level lower than 14.
-			// Adjust.OnPause();
-		}
-	}
+            // In case you are supporting API level lower than 14.
+            // Adjust.OnPause();
+        }
+    }
 }
 
 
--- iOS/Example/ViewController.cs
+++ iOS/Example/ViewController.cs
@@ -5,85 +5,85 @@ using AdjustBindingsiOS;
 
 namespace Example
 {
-	public partial class ViewController : UIViewController
-	{
-		partial void BtnTrackSimpleEvent_TouchUpInside(UIButton sender)
-		{
-			var adjustEvent = ADJEvent.EventWithEventToken("g3mfiw");
+    public partial class ViewController : UIViewController
+    {
+        partial void BtnTrackSimpleEvent_TouchUpInside(UIButton sender)
+        {
+            var adjustEvent = ADJEvent.EventWithEventToken("g3mfiw");
 
-			Adjust.TrackEvent(adjustEvent);
-		}
+            Adjust.TrackEvent(adjustEvent);
+        }
 
-		partial void BtnTrackRevenueEvent_TouchUpInside(UIButton sender)
-		{
-			var adjustEvent = ADJEvent.EventWithEventToken("a4fd35");
+        partial void BtnTrackRevenueEvent_TouchUpInside(UIButton sender)
+        {
+            var adjustEvent = ADJEvent.EventWithEventToken("a4fd35");
 
-			adjustEvent.SetRevenue(0.01, "EUR");
-			adjustEvent.SetTransactionId("dummy_id");
+            adjustEvent.SetRevenue(0.01, "EUR");
+            adjustEvent.SetTransactionId("dummy_id");
 
-			Adjust.TrackEvent(adjustEvent);
-		}
+            Adjust.TrackEvent(adjustEvent);
+        }
 
-		partial void BtnTrackCallbackEvent_TouchUpInside(UIButton sender)
-		{
-			var adjustEvent = ADJEvent.EventWithEventToken("34vgg9");
+        partial void BtnTrackCallbackEvent_TouchUpInside(UIButton sender)
+        {
+            var adjustEvent = ADJEvent.EventWithEventToken("34vgg9");
 
-			adjustEvent.AddCallbackParameter("a", "b");
-			adjustEvent.AddCallbackParameter("key", "value");
-			adjustEvent.AddCallbackParameter("a", "c");
+            adjustEvent.AddCallbackParameter("a", "b");
+            adjustEvent.AddCallbackParameter("key", "value");
+            adjustEvent.AddCallbackParameter("a", "c");
 
-			Adjust.TrackEvent(adjustEvent);
-		}
+            Adjust.TrackEvent(adjustEvent);
+        }
 
-		partial void BtnTrackPartnerEvent_TouchUpInside(UIButton sender)
-		{
-			var adjustEvent = ADJEvent.EventWithEventToken("w788qs");
+        partial void BtnTrackPartnerEvent_TouchUpInside(UIButton sender)
+        {
+            var adjustEvent = ADJEvent.EventWithEventToken("w788qs");
 
-			adjustEvent.AddPartnerParameter("x", "y");
-			adjustEvent.AddPartnerParameter("foo", "bar");
-			adjustEvent.AddPartnerParameter("x", "z");
+            adjustEvent.AddPartnerParameter("x", "y");
+            adjustEvent.AddPartnerParameter("foo", "bar");
+            adjustEvent.AddPartnerParameter("x", "z");
 
-			Adjust.TrackEvent(adjustEvent);
-		}
+            Adjust.TrackEvent(adjustEvent);
+        }
 
-		partial void BtnEnableOfflineMode_TouchUpInside(UIButton sender)
-		{
-			Adjust.SetOfflineMode(true);
-		}
+        partial void BtnEnableOfflineMode_TouchUpInside(UIButton sender)
+        {
+            Adjust.SetOfflineMode(true);
+        }
 
-		partial void BtnDisableOfflineMode_TouchUpInside(UIButton sender)
-		{
-			Adjust.SetOfflineMode(false);
-		}
+        partial void BtnDisableOfflineMode_TouchUpInside(UIButton sender)
+        {
+            Adjust.SetOfflineMode(false);
+        }
 
-		partial void BtnEnableSdk_TouchUpInside(UIButton sender)
-		{
-			Adjust.SetEnabled(true);
-		}
+        partial void BtnEnableSdk_TouchUpInside(UIButton sender)
+        {
+            Adjust.SetEnabled(true);
+        }
 
-		partial void BtnDisableSdk_TouchUpInside(UIButton sender)
-		{
-			Adjust.SetEnabled(false);
-		}
+        partial void BtnDisableSdk_TouchUpInside(UIButton sender)
+        {
+            Adjust.SetEnabled(false);
+        }
 
-		partial void BtnIsSdkEnabled_TouchUpInside(UIButton sender)
-		{
-			String message = Adjust.IsEnabled ? "SDK is ENABLED" : "SDK is DISABLED";
+        partial void BtnIsSdkEnabled_TouchUpInside(UIButton sender)
+        {
+            String message = Adjust.IsEnabled ? "SDK is ENABLED" : "SDK is DISABLED";
 
-			UIAlertView alert = new UIAlertView()
-			{
-				Title = "Is SDK enabled?",
-				Message = message
-			};
+            UIAlertView alert = new UIAlertView()
+            {
+                Title = "Is SDK enabled?",
+                Message = message
+            };
 
-			alert.AddButton("OK");
-			alert.Show();
-		}
+            alert.AddButton("OK");
+            alert.Show();
+        }
 
-		protected ViewController(IntPtr handle) : base(handle)
-		{
-			// Note: this .ctor should not contain any initialization logic.
-		}
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
 
         partial void BtnGetIds_TouchUpInside(UIButton sender)
         {
@@ -103,15 +103,15 @@ namespace Example
         }
 
         public override void ViewDidLoad()
-		{
-			base.ViewDidLoad();
-			// Perform any additional setup after loading the view, typically from a nib.
-		}
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
 
-		public override void DidReceiveMemoryWarning()
-		{
-			base.DidReceiveMemoryWarning();
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
-		}
-	}
+        }
+    }
 }
