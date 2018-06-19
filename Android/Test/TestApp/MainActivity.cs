270e9101d50071a7e1669b396385088c0673c7d4
serj <srdjan@adjust.com>
2018-06-19 15:37:54 +0200

    barebones test app & test lib

---
+++ Android/Test/TestApp/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Com.Adjust.Testlibrary;
+
+namespace TestApp
+{
+    [Activity(Label = "TestApp", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+			TestLibrary testLibrary = null;
+
+
+            button.Click += delegate { button.Text = $"{count++} clicks!"; };
+        }
+    }
+}
+
