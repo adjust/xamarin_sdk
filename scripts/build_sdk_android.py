e5c690a1702440c42c45caf0f6335cb122979b35
serj <srdjan@adjust.com>
2018-08-27 17:17:45 +0200

    build scripts pythonized

---
+++ scripts/build_sdk_android.py
@@ -0,0 +1,58 @@
+import os, subprocess
+from scripting_utils import *
+
+def build(sdk_prefix, root_dir, android_submodule_dir, with_test_lib):
+    # ------------------------------------------------------------------
+    # paths
+    sdk_adjust_dir  = '{0}/ext/android/sdk'.format(root_dir)
+    jar_in_dir      = '{0}/Adjust/adjust/build/outputs'.format(sdk_adjust_dir)
+    jar_out_dir     = '{0}/android/AdjustSdk.Xamarin.Android/Jars'.format(root_dir)
+    project_dir     = '{0}/ext/android/sdk/Adjust'.format(root_dir)
+    adjust_api_path = '{0}/Adjust/adjust/src/main/java/com/adjust/sdk/Adjust.java'.format(sdk_adjust_dir)
+
+    # ------------------------------------------------------------------
+    # Appending SDK prefix to source code ...
+    debug_green('Appending SDK prefix to source code ...')
+    replace_text_in_file(adjust_api_path,
+        'adjustInstance.onCreate(adjustConfig);', 
+        'adjustConfig.setSdkPrefix("{0}");adjustInstance.onCreate(adjustConfig);'.format(sdk_prefix))
+
+    # ------------------------------------------------------------------
+    # Running Gradle tasks: clean makeReleaseJar ...
+    debug_green('Running Gradle tasks: clean makeReleaseJar ...')
+    os.chdir(project_dir)
+    subprocess.call(['./gradlew', 'clean', 'makeReleaseJar'])
+    # flag which detects building debug jar instead ?
+    # subprocess.call(['./gradlew', 'clean', 'makeDebugJar'])
+
+    # ------------------------------------------------------------------
+    # Removing SDK prefix from source code ...
+    replace_text_in_file(adjust_api_path,
+        'adjustConfig.setSdkPrefix("{0}");adjustInstance.onCreate(adjustConfig);'.format(sdk_prefix),
+        'adjustInstance.onCreate(adjustConfig);')
+
+    # ------------------------------------------------------------------
+    # Moving the generated Android SDK JAR from ${JAR_IN_DIR} to ${JAR_OUT_DIR} ...
+    copy_files('adjust-*.jar', jar_in_dir, jar_out_dir)
+    rename_file('adjust-*.jar', 'adjust-android.jar', jar_out_dir)
+
+    if with_test_lib:
+        # ------------------------------------------------------------------
+        # Test Library paths
+        set_log_tag('ANROID-TEST-LIB-BUILD')
+        waiting_animation(duration=4.0, step=0.025)
+        debug_green('Building Test Library started ...')
+        test_jar_in_dir  = '{0}/Adjust/testlibrary/build/outputs'.format(sdk_adjust_dir)
+        test_jar_out_dir = '{0}/android/Test/TestLib/Jars'.format(root_dir)
+
+        # ------------------------------------------------------------------
+        # Running Gradle tasks: clean testlibrary:makeJar ...
+        debug_green('Running Gradle tasks: clean testlibrary:makeJar ...')
+        os.chdir(project_dir)
+        subprocess.call(['./gradlew', 'clean', ':testlibrary:makeJar'])
+
+        # ------------------------------------------------------------------
+        # Moving the generated Android SDK JAR from jar in to jar out dir ...
+        debug_green('Moving the generated Android SDK JAR from jar in to jar out dir ...')
+        copy_files('adjust-*.jar', test_jar_in_dir, test_jar_out_dir)
+        rename_file('adjust-*.jar', 'adjust-testing.jar', test_jar_out_dir)
---
+++ scripts/build_sdk_ios.py
@@ -0,0 +1,64 @@
+import os, subprocess
+from scripting_utils import *
+
+def build(sdk_prefix, root_dir, ios_submodule_dir, with_test_lib):
+    # ------------------------------------------------------------------
+    # paths
+    srcdir                  = '{0}/sdk'.format(ios_submodule_dir)
+    lib_out_dir             = '{0}/ios/AdjustSdk.Xamarin.iOS/Resources'.format(root_dir)
+    sdk_static_framework    = '{0}/Frameworks/Static/AdjustSdk.framework'.format(srcdir)
+    project_dir             = '{0}/ext/ios/sdk/Adjust'.format(root_dir)
+    adjust_api_path         = '{0}/Adjust/Adjust.m'.format(srcdir)
+
+    # ------------------------------------------------------------------
+    # Removing old iOS SDK binary
+    debug_green('Removing old iOS SDK binary ...')
+    if os.path.exists('{0}/libAdjust.a'):
+        os.remove('{0}/libAdjust.a')
+
+    # ------------------------------------------------------------------
+    # Appending SDK prefix to source code
+    debug_green('Appending SDK prefix to source code ...')
+    replace_text_in_file(adjust_api_path,
+        'self.activityHandler = [ADJAdjustFactory activityHandlerWithConfig:adjustConfig', 
+        '[adjustConfig setSdkPrefix:@"{0}"];self.activityHandler = [ADJAdjustFactory activityHandlerWithConfig:adjustConfig'.format(sdk_prefix))
+
+    # ------------------------------------------------------------------
+    # Building new iOS SDK binary
+    debug_green('Building new iOS SDK binary ...')
+    os.chdir(srcdir)
+    subprocess.call(['xcodebuild', '-target', 'AdjustStatic', '-configuration', 'Release', 'clean', 'build'])    
+
+    # ------------------------------------------------------------------
+    # Removing SDK prefix from source code
+    debug_green('Removing SDK prefix from source code ...')
+    replace_text_in_file(adjust_api_path,
+        '[adjustConfig setSdkPrefix:@"{0}"];self.activityHandler = [ADJAdjustFactory activityHandlerWithConfig:adjustConfig'.format(sdk_prefix),
+        'self.activityHandler = [ADJAdjustFactory activityHandlerWithConfig:adjustConfig')
+
+    # ------------------------------------------------------------------
+    # Copying the generated binary  to lib out dir
+    debug_green('Copying the generated binary to {0} ...'.format(lib_out_dir))
+    copy_file(sdk_static_framework + '/Versions/A/AdjustSdk', lib_out_dir + '/libAdjust.a')
+
+    if with_test_lib:
+        # ------------------------------------------------------------------
+        # Test Library paths
+        set_log_tag('IOS-TEST-LIB-BUILD')
+        debug_green('Building Test Library started ...')
+        waiting_animation(duration=4.0, step=0.025)
+        test_static_framework   = '{0}/Frameworks/Static/AdjustTestLibrary.framework'.format(srcdir)
+        test_lib_out_dir        = '{0}/ios/Test/TestLib/Resources'.format(root_dir)
+
+        # ------------------------------------------------------------------
+        # Removing old iOS SDK binary
+        debug_green('Removing old iOS SDK binary ...')
+        if os.path.exists('{0}/libAdjustTest.a'):
+            os.remove('{0}/libAdjustTest.a')
+
+        # ------------------------------------------------------------------
+        # Building new iOS SDK binary
+        debug_green('Building new iOS SDK binary ...')
+        os.chdir('{0}/AdjustTests/AdjustTestLibrary'.format(srcdir))
+        subprocess.call(['xcodebuild', '-target', 'AdjustTestLibraryStatic', '-configuration', 'Release', 'clean', 'build'])
+        copy_file(test_static_framework + '/Versions/A/AdjustTestLibrary', test_lib_out_dir + '/libAdjustTest.a')
