0213a8a2373910311d6144d7dda097d0444e570f
nonelse <pedro@adjust.com>
2020-10-22 17:43:42 +0200

    Import iOs frameworks

--- ios/AdjustSdk.Xamarin.iOS/ApiDefinition.cs
+++ ios/AdjustSdk.Xamarin.iOS/ApiDefinition.cs
@@ -1,11 +1,27 @@
 ﻿using System;
-
 using Foundation;
 using ObjCRuntime;
 
 namespace AdjustBindingsiOS
 {
-	// This declares the callback signature for the block:
+	internal static class FrameworkImports {
+
+		static readonly Type AdSupportType;
+		static readonly Type iAdType;
+		static readonly Type CoreTelephonyType;
+		static readonly Type StoreKitType;
+		static readonly Type AppTrackingTransparencyType;
+
+		static FrameworkImports()
+        {
+			AdSupportType = typeof(AdSupport.ASIdentifierManager);
+			iAdType = typeof(iAd.ADClient);
+			CoreTelephonyType = typeof(CoreTelephony.CTCarrier);
+			StoreKitType = typeof(StoreKit.SKAdNetwork);
+			AppTrackingTransparencyType = typeof(AppTrackingTransparency.ATTrackingManager);
+		}
+	}
+
 	public delegate void RequestTrackingAuthorizationCompletionHandler(nuint status);
 
 	[BaseType(typeof(NSObject))]
